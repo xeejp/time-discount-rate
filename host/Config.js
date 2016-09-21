fdb61a610525790c5c62d5b91d00f138110dfa2b
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-09-21 15:55:46 +0900

    fix UI

--- host/Config.js
+++ host/Config.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux'
 
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ActionSettings from 'material-ui/svg-icons/action/settings'
 import SnackBar from 'material-ui/SnackBar'
-import { Card, CardHeader, CardText } from 'material-ui/Card'
-import { upadateConfig } from './actions'
+import Dialog from 'material-ui/Dialog'
+import { upadateConfig, fetchContents } from './actions'
 
 const mapStateToProps = ({ money, unit, page }) => ({
         money, unit, page
@@ -20,9 +22,37 @@ class Config extends Component {
             value2: unit,
 			isValid1: true,
             isValid2: true,
-			snack: false
+            open: false,
+			snack: false,
+            message: "設定を送信しました。"
         }
 	}
+
+    handleOpen() {
+        const { dispatch } = this.props
+        dispatch(fetchContents())
+        this.setState({
+           value1: this.props.money+'',
+           value2: this.props.unit,
+           isValid1: true,
+           isValid2: true,
+           open: true,
+        })
+    }
+
+    handleClose() {
+        this.setState({ open: false })
+    }
+
+    reset() {
+        this.setState({
+            open: false,
+            snack: true,
+            message: "設定を初期化しました。"
+        })
+        const { dispatch } = this.props
+        dispatch(upadateConfig({money: 7500, unit: '円'}))
+    }
 
     closeSnack() {
     		this.setState({
@@ -54,55 +84,71 @@ class Config extends Component {
 		const { dispatch } = this.props
 		const { value1,value2 } = this.state
 		this.setState({
-      		snack: true
+      		snack: true,
+            open: false,
+            message: "設定を送信しました。"
 		})
 		dispatch(upadateConfig({money:parseInt(value1,10),unit:value2}))
 	}
 
   render() {
-    const { snack, isValid1, isValid2} = this.state
+    const { snack, isValid1, isValid2, message} = this.state
     const {money, unit, page} = this.props
+    const actions = [
+        <RaisedButton
+          primary={true}
+          label= {"適用"}
+          disabled={!isValid1 || !isValid2 || page != 'waiting'}
+          onTouchTap={this.handleClick.bind(this)}
+        />,
+        <RaisedButton
+          label={"キャンセル"}
+          onTouchTap={this.handleClose.bind(this)}
+        />,
+        <RaisedButton
+          label={"初期化"}
+          onTouchTap={this.reset.bind(this)}
+        />
+    ]
     console.log("AAAAAAAAAAAAA")
-    return (<Card>
-          <CardHeader
+    return (
+        <span>
+        <FloatingActionButton
+            onClick={this.handleOpen.bind(this)}
+            disabled={page != "waiting"}
+        >
+            <ActionSettings />
+        </FloatingActionButton>
+        <Dialog
             title={"オプション"}
-            actAsExpander={true}
-            showExpandableButton={true}
+            actions={actions}
+            model={false}
+            open={this.state.open}
+            autoScrollBodyContent={true}
+        >
+          <p>現在の元金:{money} 現在の単位: {unit}</p>
+          <TextField
+            floatingLabelText="元金(100以上の整数)"
+            value={this.state.value1}
+            onChange={this.handleChange1.bind(this)}
+            disabled={page != 'waiting'}
           />
-          <CardText expandable={true}>
-                <p>現在の元金:{money} 現在の単位: {unit}</p>
-                <TextField
-                    floatingLabelText="元金(100以上の整数)"
-                    value={this.state.value1}
-                    onChange={this.handleChange1.bind(this)}
-                    disabled={page != 'waiting'}
-                />
-                <br/>
-                <TextField
-                    floatingLabelText= "単位"
-                    value={this.state.value2}
-                    onChange={this.handleChange2.bind(this)}
-                    disabled={page != 'waiting'}
-                />
-                <br/>
-                <RaisedButton
-			        primary={true}
-			        label= {"適用"}
-			        disabled={!isValid1 || !isValid2 || page != 'waiting'}
-			        onClick = {this.handleClick.bind(this)}
-                    style = {{
-             		    float:"right"
-           		    }}
-			    />
-                <br/>
-                <SnackBar
-          		   open={snack}
-          		   message={"適用しました。"}
-          		   autoHideDuration={3000}
-          	 	   onRequestClose={this.closeSnack.bind(this)}
-        		/>
-          </CardText>
-    </Card>)
+          <br/>
+          <TextField
+            floatingLabelText= "単位"
+            value={this.state.value2}
+            onChange={this.handleChange2.bind(this)}
+            disabled={page != 'waiting'}
+          />
+        </Dialog>
+        <SnackBar
+           open={snack}
+          message={message}
+          autoHideDuration={3000}
+          onRequestClose={this.closeSnack.bind(this)}
+        />
+     </span>
+    )
   }
 }
 
